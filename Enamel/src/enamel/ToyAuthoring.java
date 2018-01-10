16344c3a748824ea535ddcf9a0db8ae6306d122a
danielkozlovsky <daniel.kozlovsky@gmail.com>
2018-01-10 13:19:25 -0500

    Test Commit and push to check configurations

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -2,6 +2,7 @@ package enamel;
 
 public class ToyAuthoring {
 
+	// will be replaced by Authoring App
     public static void main(String[] args) { 	  	
     	    ScenarioParser s = new ScenarioParser(true);
     	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
